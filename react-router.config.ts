f8ad55dc27510ab386b6fb68cf7031dbbaab3258
Ryan Chia <ryan_chia8@hotmail.com>
2026-07-10 08:04:42 +0000

    change to static site

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,9 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
+  
+  prerender : async function () {
+    return ["/"]
+  },
 } satisfies Config;
